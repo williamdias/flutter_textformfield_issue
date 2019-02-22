d44ef6a2712105bcbacdc17ef749c540bfc36fbf
rahuvich <raulmateob@gmail.com>
2019-02-22 12:06:34 +0100

    removed 1 line

--- lib/mockPage.dart
+++ lib/mockPage.dart
@@ -8,14 +8,11 @@ class MockPage extends StatefulWidget {
 class _MockPageState extends State<MockPage> {
   @override
   Widget build(BuildContext context) {
-    FocusScope.of(context).requestFocus(new FocusNode());
-
     return Scaffold(
       appBar: AppBar(),
       body: Column(
         children: <Widget>[
           TextField(
-            autofocus: true,
           ),
           IconButton(
             icon: Icon(Icons.arrow_forward_ios),
