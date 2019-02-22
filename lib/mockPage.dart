6e23c465bc4bcc205235a3755ee7c52726cfad02
rahuvich <raulmateob@gmail.com>
2019-02-22 11:45:29 +0100

    Second commit

---
+++ lib/mockPage.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MockPage extends StatefulWidget {
+  @override
+  _MockPageState createState() => _MockPageState();
+}
+
+class _MockPageState extends State<MockPage> {
+  @override
+  Widget build(BuildContext context) {
+    FocusScope.of(context).requestFocus(new FocusNode());
+
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            autofocus: true,
+          ),
+          IconButton(
+            icon: Icon(Icons.arrow_forward_ios),
+            onPressed: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => MockPage(),
+                ),
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
